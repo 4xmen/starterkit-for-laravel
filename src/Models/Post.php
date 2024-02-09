6941dcee58842a055ed6f5d22aabdfe61ccfeecb
A1Gard <a1gard@4xmen.ir>
2024-02-09 06:45:42 +0330

    fixed bug subtitle

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -80,7 +80,7 @@ class Post extends Model implements HasMedia
 {
     use SoftDeletes, InteractsWithMedia, Taggable, HasTrixRichText, Searchable,HasTranslations;
 
-    public $translatable = ['title','sub_title','body'];
+    public $translatable = ['title','subtitle','body'];
 
     public function categories()
     {
